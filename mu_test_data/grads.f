79405bf4053eb8423013c1396e1fb1a9d01c1f21
AlexlwAstro <alexander.lw@me.com>
2018-03-26 17:20:13 +0100

    X and rho outputs from diffusion equation

--- mu_test_data/grads.f
+++ mu_test_data/grads.f
@@ -16,13 +16,12 @@ C SUBROUTINE TO AVOID GRADIENT NANS DUE TO 0/0
       END
 
       PROGRAM GRADS
-c      subroutine gradient
 
       IMPLICIT REAL*8(A-H,O-Z)
 C      include 'CODE_alw/maincom.2p1'
       include 'consts.2p1'
-      character*30 abund_in,model_in,head_in,file_out
-      integer*4 kcount,MAXME,NMODEL,KMESH,MESH,x,xm,xp
+      character*30 abund_in,model_in,head_in,file_out,eq_out
+      integer*4 kcount,MAXME,NMODEL,KMESH,MESH
       PARAMETER (MAXME=1424)
       real*8 Kthm
       Real*8 XI(MAXME),XHE3(MAXME),XHE4(MAXME),XC(MAXME),XN(MAXME),
@@ -31,40 +30,44 @@ C      include 'CODE_alw/maincom.2p1'
      # MFRAC(MAXME),rfrac(MAXME),logP(MAXME),logT(MAXME),
      # logDens(MAXME),lum(MAXME),enuc(MAXME),egrav(MAXME),
      # eneu(MAXME),DELTA(MAXME),RADIAT(MAXME),ADIAB(MAXME),
-     # VSOUND(MAXME),opac(maxme),cp(maxme)
+     # VSOUND(MAXME),opac(maxme),cp(maxme),RHO_PHYS(MAXME),
+     # RHO_N(MAXME)
 c      WRITE(*,*) 'HELLO WORLD'
-      
+
+
+C NOTE ABOUT READING:
+C For '_model' files, copy from NoMachine FIRST, then delete all
+C strings from the file BEFORE running grads2p1p4.go
+
 c    CHARACTER(*),PARAMETER :: fileplace = "FeHp006_alw/mu_test_data/"
       abund_in = 'diff_mu_logL=2_1231_abund'
       model_in = 'diff_mu_logL=2_1231_model'
 C      head_in = 'diff_mu_logL=2_1231_header'
       file_out = 'diff_mu_logL=2_1231_grads'
+      eq_out = 'diff_mu_logL=2_1231_eq'
 C fileplace//      
 c      OPEN(UNIT=21,FILE=head_in,status='OLD')
       OPEN(UNIT=28,FILE=abund_in,status='OLD')
       OPEN(UNIT=29,FILE=model_in,status='OLD')
       OPEN(UNIT=38,FILE=file_out,status='REPLACE')
+      OPEN(UNIT=44,FILE=eq_out,status='REPLACE')
       
-C      read(21,180) MAXME,NMODEL
-C       180  FORMAT(117X, 2I5)
-C      WRITE(*,*) MAXME,',',NMODEL
+      WRITE(*,*) file_out
       write(38,110)
       write(38,120)
- 110  FORMAT(1H1,/,'      Mr            MU        d(mu)/dr_2    dln(mu)/   
-     &dr_2 dln(mu)/dln(p)_2   d(X_Li7)/dr_2   d(X_C)/dr_2  d(X_N)/dr_2   
-     &  d(X_He3)/dr_2  d(X_He4)/dr_2    Dthm      Dthm_0     MESH')
+ 110  FORMAT(1H1,/,'      Mr            MU           d(mu)/dr       dln(
+     &mu)/dr   dln(mu)/dln(p)  d(X_Li7)/dr   d(X_C)/dr      d(X_N)/dr  
+     &   d(X_He3)/dr     d(X_He4)/dr    Dthm_0   MESH')
 
-Cx1mas,MU,DMU_DR_2,DLNMU_DR_2,DEL_MU_2,
-C     # DXLi7_DR_2,DXC_DR_2,DXN_DR_2,KM
+Cx1mas,MU,DMU_DR ,DLNMU_DR ,DEL_MU ,
+C     # DXLi7_DR ,DXC_DR ,DXN_DR ,KM
  120  FORMAT('==========================================================
      #==================================================================
-     #=====================================================')
-c      IF (K .EQ. 1) THEN
+     #===========================================================')
 
-c      END IF
       kcount = 0
 C READING DO-LOOP
-C 
+ 
       DO K = 1,MAXME
        READ(28,100,end=40) x1mas(k),XI(k),XD(k),XHE3(k),XHE4(k),
      # XLi7(k),XBe7(k),XC(k),XC13(k),XN(k),XN15(k),XO(k),
@@ -82,7 +85,7 @@ C       write(*,*) x1mas(k),',',XI(k),',',mfrac(k),',',rfrac(k)
        kcount = kcount + 1
       END DO
    40 WRITE(*,*)'Reading finished: number of lines: ', kcount
-      WRITE(*,*) x1mas(1)
+C      WRITE(*,*) x1mas(1)
 
 C CALCULATION DO-LOOP
       DO K = 1,MAXME
@@ -92,14 +95,14 @@ C CALCULATION DO-LOOP
 
 C    CALCULATE FINVMU = INVERSE OF FINAL_MU = 1/MU = 1/MU_I + 1/MU_E
 C SO: 1/MU = SUM_I ((Z_I + 1)*X_I/A_I), ASSUMING FULL IONIZATION
-C N.B.: FINVMU DEFINED AT KM = K - 1
-      FINVMU = (XI(km)*2/1)+(XHE3(km)*3/3)+(XHE4(km)*3/4)+(XC(km)*7/12)
-     $ +(XN(km)*8/14)+(XO(km)*9/16)+(XD(km)*2/2)+(XLi7(km)*4/7)
-     $ +(XBe7(km)*5/7)+(XC13(km)*7/13)+(XN15(km)*8/16)+(XO17(km)*9/17)
-     $ +(XO18(km)*9/18)+(XFe56(km)*26/56)
+C N.B.: FINVMU_B DEFINED AT KM = K - 1
+      FINVMU_B = (XI(km)*2/1)+(XHE3(km)*3/3)+(XHE4(km)*3/4)
+     $ +(XC(km)*7/12)+(XN(km)*8/14)+(XO(km)*9/16)+(XD(km)*2/2)
+     $ +(XLi7(km)*4/7)+(XBe7(km)*5/7)+(XC13(km)*7/13)+(XN15(km)*8/16)
+     $ +(XO17(km)*9/17)+(XO18(km)*9/18)+(XFe56(km)*26/56)
 
-C CALCULATE MEAN MOLECULAR WEIGHT, FINAL_MU, BY INVERTING FINVMU
-      FINAL_MU = 1/FINVMU
+C CALCULATE MEAN MOLECULAR WEIGHT, FINAL_MU_B, BY INVERTING FINVMU
+      FINAL_MU_B = 1/FINVMU_B
 
 C DEFINE FINVMU_K, THE EQUIVALENT OF FINVMU, BUT AT K = KM + 1
 C errorbait
@@ -111,112 +114,67 @@ C errorbait
 C IN THE SAME WAY, DEFINE FINAL_MU_K
       FINAL_MU_K = 1/FINVMU_K
 
-C DEFINE FINVMU_P, THE EQUIVALENT OF FINVMU, BUT AT KP = K + 1
-      FINVMU_P = (XI(kp)*2/1)+(XHE3(kp)*3/3)+(XHE4(kp)*3/4)
+C DEFINE FINVMU_F, THE EQUIVALENT OF FINVMU, BUT AT KP = K + 1
+      FINVMU_F = (XI(kp)*2/1)+(XHE3(kp)*3/3)+(XHE4(kp)*3/4)
      $ +(XC(kp)*7/12)+(XN(kp)*8/14)+(XO(kp)*9/16)+(XD(kp)*2/2)
      S +(XLi7(kp)*4/7)+(XBe7(kp)*5/7)+(XC13(kp)*7/13)+(XN15(kp)*8/16)
      $ +(XO17(kp)*9/17)+(XO18(kp)*9/18)+(XFe56(kp)*26/56)
 
-C IN THE SAME WAY, DEFINE FINAL_MU_P
-      FINAL_MU_P = 1/FINVMU_P
+C IN THE SAME WAY, DEFINE FINAL_MU_F
+      FINAL_MU_F = 1/FINVMU_F
 
 C TAKE DIFFERENCE OF THE MU'S
-C 1-MESH POINT WIDTH DERIVATIVE METHOD
-C      DMU = FINAL_MU_K - FINAL_MU
-C 1-MESH POINT WIDTH DERIVATIVE METHOD
-C      DMU_P = FINAL_MU_P - FINAL_MU_K
-C 2-POINT WIDTH METHOD
-      DMU_2 = FINAL_MU_P - FINAL_MU
+C CENTRAL DERIVATIVE METHOD - CHOSEN METHOD FOR SINGLE DERIVATIVE
+      DMU = FINAL_MU_F - FINAL_MU_B
 
 C Focus on individual isotopes
-C 1-POINT METHOD
-C      DXC = XC(K) - XC(KM)
-C      DXLi7 = XLi7(K) - XLi7(KM)
-C      DXN = XN(K) - XN(KM)
-C 1-POINT METHOD
-C      DXC_P = XC(KP) - XC(K)
-C      DXLi7_P = XLi7(KP) - XLi7(K)
-C      DXN_P = XN(KP) - XN(K)
-C 2-POINT METHOD
-      DXC_2 = XC(KP) - XC(KM)
-      DXLi7_2 = XLi7(KP) - XLi7(KM)
-      DXN_2 = XN(KP) - XN(KM)
-      DXHE3_2 = XHE3(KP) - XHE3(KM)
-      DXHE4_2 = XHE4(KP) - XHE4(KM)
+C CENTRAL DERIVATIVE METHOD
+      DXC = XC(KP) - XC(KM)
+      DXLi7 = XLi7(KP) - XLi7(KM)
+      DXN = XN(KP) - XN(KM)
+      DXHE3 = XHE3(KP) - XHE3(KM)
+      DXHE4 = XHE4(KP) - XHE4(KM)
 
 C CALCULATE THE (LINEAR) MOLECULAR GRADIENT
-C GET THE KTH-LAYER WIDTHS FOR DIFFERENT PARAMETERS
-C 1-MESH POINT WIDTH DERIVATIVE METHOD -
-C      DR = (2.21E+01)*(RFRAC(K)-RFRAC(KM))
-C      DLNP = DLOG(10**logP(K)) - DLOG(10**logP(KM))
-C      DLOGP = logP(K)-logP(KM)
-C      DLNT = DLOG(10**logT(K)) - DLOG(10**logT(KM))
-C      DLOGT = logT(K)-logT(KM)
-C      DMASS = ()/emtot
-C 1-MESH POINT WIDTH DERIVATIVE METHOD +
-C      DR_P = (2.21E+01)*(RFRAC(KP)-RFRAC(K))
-C      DLNP_P = DLOG(10**logP(KP)) - DLOG(10**logP(K))
-C      DLOGP_P = logP(kp)-logP(k)
-C      DLNT_P = DLOG(10**logT(KP)) - DLOG(10**logT(K))
-C      DLOGT_P = logT(kp)-logT(k)
-C      DMASS_P = ()/emtot
-C 2-POINT WIDTH METHOD
-      DR_2 = (2.21E+01)*(RFRAC(KP)-RFRAC(KM))
-      DLNP_2 = DLOG(10**logP(KP)) - DLOG(10**logP(KM))
-      DLOGP_2 = logP(kp)-logP(km)
-      DLNT_2 = DLOG(10**logT(KP)) - DLOG(10**logT(KM))
-      DLOGT_2 = logT(kp)-logT(km)
-c      DMASS_2 = ()
+C BY GETTING THE KTH-LAYER WIDTHS FOR DIFFERENT PARAMETERS
+
+C CENTRAL DERIVATIVE METHOD
+      DR = (2.21E+01)*(RFRAC(KP)-RFRAC(KM))
+      DLNP = DLOG(10**logP(KP)) - DLOG(10**logP(KM))
+      DLOGP = logP(kp)-logP(km)
+      DLNT = DLOG(10**logT(KP)) - DLOG(10**logT(KM))
+      DLOGT = logT(kp)-logT(km)
+C      DMASS = ()
 
 
 C TAKE DIFFERENCE OF NATURAL LOG OF THE MU'S
-C 1-POINT METHOD -
-C      DLNMU = DLOG(FINAL_MU_K) - DLOG(FINAL_MU)
-C      DLOGMU = DLOG10(FINAL_MU_K) - DLOG10(FINAL_MU)
-C 1-POINT METHOD -
-C      DLNMU_P = DLOG(FINAL_MU_P) - DLOG(FINAL_MU_K)
-C      DLOGMU_P = DLOG10(FINAL_MU_P) - DLOG10(FINAL_MU_K)
-C 2-POINT METHOD
-      DLNMU_2 = DLOG(FINAL_MU_P) - DLOG(FINAL_MU)
-      DLOGMU_2 = DLOG10(FINAL_MU_P) - DLOG10(FINAL_MU)
+C BACKWARD DERIVATIVE METHOD
+C      DLNMU_B = DLOG(FINAL_MU_K) - DLOG(FINAL_MU_B)
+C      DLOGMU_B = DLOG10(FINAL_MU_K) - DLOG10(FINAL_MU_B)
+C FORWARD DERIVATIVE METHOD
+C      DLNMU_F = DLOG(FINAL_MU_F) - DLOG(FINAL_MU_K)
+C      DLOGMU_F = DLOG10(FINAL_MU_F) - DLOG10(FINAL_MU_K)
+C CENTRAL DERIVATIVE METHOD
+      DLNMU = DLOG(FINAL_MU_F) - DLOG(FINAL_MU_B)
+      DLOGMU = DLOG10(FINAL_MU_F) - DLOG10(FINAL_MU_B)
 
 C DEFINE RELEVANT LINEAR GRADIENTS
-C 1-POINT METHOD (K-1 to K)
-C      DMU_DR = DMU/DR ! d(mu)/dr
-C      DMU_DM = DMU/DMASS ! d(mu)/dm
-C      DEL_MU = DLNMU/DLNP ! d(ln(mu))/d(ln(P))
-C      DLNMU_DR = DLNMU/DR ! d(ln(mu))/dr
-C      DXC_DR = DXC/DR
-C      DXN_DR = DXN/DR
-C      DXLi7_DR = DXLi7/DR
-
-C DEFINE RELEVANT LINEAR GRADIENTS
-C 1-POINT METHOD (K to K+1)
-C      DMU_DR_P = DMU_P/DR_P ! d(mu)/dr
-C      DMU_DM_P = DMU/DMASS ! d(mu)/dm
-C      DEL_MU_P = DLNMU_P/DLNP_P ! d(ln(mu))/d(ln(P))
-C      DLNMU_DR_P = DLNMU_P/DR_P ! d(ln(mu))/dr
-C      DXC_DR_P = DXC_P/DR_P
-C      DXN_DR_P = DXN_P/DR_P
-C      DXLi7_DR_P = DXLi7_P/DR_P
-
-
-C 2-POINT METHOD
-      CALL AVOIDNAN(DMU_DR_2,DMU_2,DR_2) ! d(mu)/dr
-C      CALL AVOIDNAN(DMU_DM_2,DMU_2,DMASS_2) ! d(mu)/dm
-      CALL AVOIDNAN(DEL_MU_2,DLNMU_2,DLNP_2) ! d(ln(mu))/d(ln(P))
-      CALL AVOIDNAN(DLNMU_DR_2,DLNMU_2,DR_2) ! d(ln(mu))/dr
-      CALL AVOIDNAN(DXC_DR_2,DXC_2,DR_2) ! C12
-      CALL AVOIDNAN(DXN_DR_2,DXN_2,DR_2) ! N14
-      CALL AVOIDNAN(DXLi7_DR_2,DXLi7_2,DR_2) ! Li7
-      CALL AVOIDNAN(DXHE3_DR_2,DXHE3_2,DR_2) ! He3
-      CALL AVOIDNAN(DXHE4_DR_2,DXHE4_2,DR_2) ! He4
+C CENTRAL DERIVATIVE METHOD
+      CALL AVOIDNAN(DMU_DR,DMU,DR) ! d(mu)/dr
+C      CALL AVOIDNAN(DMU_DM,DMU,DMASS) ! d(mu)/dm
+      CALL AVOIDNAN(DEL_MU,DLNMU,DLNP) ! d(ln(mu))/d(ln(P))
+      CALL AVOIDNAN(DLNMU_DR,DLNMU,DR) ! d(ln(mu))/dr
+      CALL AVOIDNAN(DXC_DR,DXC,DR) ! C12
+      CALL AVOIDNAN(DXN_DR,DXN,DR) ! N14
+      CALL AVOIDNAN(DXLi7_DR,DXLi7,DR) ! Li7
+      CALL AVOIDNAN(DXHE3_DR,DXHE3,DR) ! He3
+      CALL AVOIDNAN(DXHE4_DR,DXHE4,DR) ! He4
 
 C THERMOHALINE MIXING IMPLEMENTATION
 C LEDOUX CRITERION (CANTIELLO, LANGER, 2010):
 C  0 >= DEL-DEL_AD >= (PHI/DELTA)*DEL_MU
-C WITH PHI = PART(DLN(RHO)/DLN(MU))_P,T
-c AND DELTA = PART(DLN(RHO)/DLN(T))_P,MU
+C WITH PHI = PART(DLN(RHO)/DLN(MU))_F,T
+c AND DELTA = PART(DLN(RHO)/DLN(T))_F,MU
 
 c Calculate thermohaline diffusion coefficient
 C conversions to get physical parameters in cgs units
@@ -234,32 +192,122 @@ c First, get thermal diffusivity (cgs units)
 C Final result (Cantiello & Langer (2010) & Maeder et al. (2013))
 C with (phi/delta) = 1 (must always be +ve from first principles)
       Cthl = 1000 ! Maeder et al. (2013), from Ulrich (1972)
-C If loop comes from Lattanzio et al. (2015): D = 0 unless del_mu < 0
-      if ((DEL_MU_2 .LT. 0) .AND. (DEL_MU_2 .GT. (RADIAT(K)-ADIAB(K))))
-     # then
-        Dthl = Cthl*Kthm*(DEL_MU_2)/(RADIAT(K)-ADIAB(K))
-      else
-        Dthl = 0.0
-      end if
+C If loop comes from Lattanzio et al. (2015): D = 0 unless del_MU < 0
+C      if ((DEL_MU .LT. 0) .AND. (DEL_MU .GT. (RADIAT(K)-ADIAB(K))))
+C     # then
+C        Dthl = Cthl*Kthm*(DEL_MU)/(RADIAT(K)-ADIAB(K))
+C      else
+C        Dthl = 0.0
+C      end if
 
-      if (DEL_MU_2 .LT. 0) then
-        Dthl_0 = Cthl*Kthm*(DEL_MU_2)/(RADIAT(K)-ADIAB(K))
+      if (DEL_MU .LT. 0) then
+        Dthl_0 = Cthl*Kthm*(DEL_MU)/(RADIAT(K)-ADIAB(K))
       else
         Dthl_0 = 0.0
       end if
 
+C DIFFUSION EQUATION CALCULATION - d2(X_i)/dr2 construction (linear)
+C using concept & equations from:
+C https://me.ucsb.edu/~moehlis/APC591/tutorials/tutorial5/node3.html
+C and:
+C www.math.utep.edu/Faculty/sewell/tamu/664/prob4b.pdf
+
+C diffusion equation uses number density as the variable being
+C differetiated - comes from continuity equation. For 1 species, with
+C no fusion modelled, can represent the number density with (X_i)*rho,
+C since each particle of the species has a constant mass
+
+C First, get the physical density from the log(rho) data from stampe
+C and convert to element-specific masses: test case = N14
+      RHO_N(KM) = XN(KM)*(10**logDens(KM))
+      RHO_N(K) = XN(K)*(10**logDens(K))
+      RHO_N(KP) = XN(KP)*(10**logDens(KP))
+
+C Second, calulate the numerator
+C Numerator = difference between forward and backward differentials
+C i.e., (dT/dr)_(k+1/2) - (dT/dr)_(k-1/2)
+C Define backward, forward difference in T
+      DRHO_N_B = RHO_N(K) - RHO_N(KM)
+      DRHO_N_F = RHO_N(KP) - RHO_N(K)
+
+      DXN_F = XN(KP) - XN(K)
+      DXN_B = XN(K) - XN(KM)
+
+C Define backward, forward differences in physical distance, in this
+C (1D) case the radius, r
+      DR_B = (2.21E+01*SUNRcm)*(rfrac(k) - rfrac(km))
+      DR_F = (2.21E+01*SUNRcm)*(rfrac(kp) - rfrac(k))
+
+C Divide to get the numerator components, then take their difference
+C to get the final numerator
+      CALL AVOIDNAN(DRHO_N_DR_B,DRHO_N_B,DR_B)
+      CALL AVOIDNAN(DRHO_N_DR_F,DRHO_N_F,DR_F)
+      D2RHO_N_DR = DRHO_N_DR_F - DRHO_N_DR_B
+
+      CALL AVOIDNAN(DXN_DR_B,DXN_B,DR_B)
+      CALL AVOIDNAN(DXN_DR_F,DDXN_F,DR_F)
+      D2XN_DR = DXN_DR_F - DXN_DR_B
+
+C Denominator = central difference for r
+      DR_2 = (DR*SUNRcm)/2
+
+C Combine the numerator and denominator to obtain the
+C second-order differential
+      CALL AVOIDNAN(D2RHO_N_DR2,D2RHO_N_DR,DR_2)
+
+      CALL AVOIDNAN(D2XN_DR2,D2XN_DR,DR_2)
+
+C timestep calculation, using logL = 2.1231 and ages at (k-1),(k+1),
+C then divide by 2 to approximate to (k-1/2),(k+1/2), then divide by 100
+c      dtime_yr = ((10**10.10616736) - (10**10.10616654))/(2*100)
+c      dtime_s = dtime_yr*secanni
+
+C get a (fixed) test value of D_therm for the equation
+      Dthm_eq = 0.1457D+06
+
+C Diffusion equation - gives change in physical
+C density rho_N(r,t) due to time only, in cgs
+      dRHO_N_dt = Dthm_eq*D2RHO_N_DR2
+C get 'normalising' time, tau = Rtot^2/D in years
+      t_norm = ((2.21E+01*SUNRcm)**2)/(Dthm_eq*secanni)
+
+C write out numerical results
       if (k .eq. MAXME) then
 	write(*,*) 'km = (MAXME-1) = ', km
-        
+        write(*,*) RHO_N(km),RHO_N(k)
       end if
-      write(38,102) x1mas(K),FINAL_MU_K,DMU_DR_2,DLNMU_DR_2,DEL_MU_2,
-     # DXLi7_DR_2,DXC_DR_2,DXN_DR_2,DXHE3_DR_2,DXHE4_DR_2,Dthl,Dthl_0,K
+
+      write(38,102) x1mas(K),FINAL_MU_K,DMU_DR,DLNMU_DR,DEL_MU,
+     # DXLi7_DR,DXC_DR,DXN_DR,DXHE3_DR,DXHE4_DR,Dthl_0,K
+      
+      if (k .eq. 1) then
+       write(44,103) Dthm_eq,t_norm
+       write(44,104)
+       write(44,105)
+      end if
+
+      if (dr_2 .eq. 0.0) then
+       write(*,*) rfrac(kp),rfrac(km)
+      end if
+
+      write(44,106) x1mas(K),rfrac(k),XN(k),D2XN_DR2,DRHO_N_F,DRHO_N_B,
+     # DR_2,D2RHO_N_DR,D2RHO_N_DR2,dRHO_N_dt,K
+
  100  FORMAT(1X,F12.9,14D14.7,105X,I5)
  101  FORMAT(F12.9,F10.7,F10.6,F8.5,F8.4,D13.5,3D11.3,D9.2,
      &D11.3,F6.3,D12.4,D12.4,D9.2,I5)
- 102  FORMAT(1X,F12.9,9D15.7,2D12.4,I5)
+ 102  FORMAT(1X,F12.9,9D15.7,D12.4,I5)
 
-c ,XI(kP),XI(kM),KP ,2D15.7,I5 ,DR_2,DLNP_2
+ 103  FORMAT('Dthm = ',D11.4,', normalisation time = ',D11.4)
+ 104  FORMAT('      Mr         R/R*         X_N          d2X_N/dr2      
+     #drho_N_rf       drho_N_rb        dr          d2rho_N/dr    d2rho_N
+     #/dr2    drho_N/dt   MESH')
+ 105  FORMAT('==========================================================
+     #==================================================================
+     #=========================')
+ 106  FORMAT(1X,F12.9,F10.7,8D15.7,I5)
+
+c ,XI(kP),XI(kM),KP ,2D15.7,I5 ,DR ,DLNP_2
       
       enddo
    
